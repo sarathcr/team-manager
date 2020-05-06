647f9632eb1242e6240972187da21f96c4137bc9
justinthomask <justinthomask@opentrends.net>
2020-05-06 15:44:26 +0200

    Merge branch 'develop' of https://gitlab.opentrends.net/tk004-thinko_elearning/tk004-thinko_creator into feature/TA-731-Home-page
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	src/app/app.component.html
    #	src/app/app.module.ts

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -1,19 +1,21 @@
 import { ListProjectComponent } from './creator-home/list-project/list-project.component';
 import { AddProjectComponent } from './creator-home/add-project/add-project.component';
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// import { CreatorHomeModule } from './creator-home/creator-home.module';
 import { ProjectEditorHeaderComponent } from './creator-projectEditor/project-editor-header/project-editor-header.component';
 import { ProjectEditorSidebarComponent } from './creator-projectEditor/project-editor-sidebar/project-editor-sidebar.component';
 
 @NgModule({
-    declarations:[
+    declarations: [
         AddProjectComponent,
         ListProjectComponent,
         ProjectEditorHeaderComponent,
         ProjectEditorSidebarComponent
     ],
-    imports:[
-        CommonModule
+    imports: [
+        CommonModule,
+        // CreatorHomeModule,
     ],
     exports: [
         AddProjectComponent,
---
+++ src/app/services/localStorage.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+export interface Project {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  theme: string;
+  status: string;
+  threads: string[];
+  date: Date;
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+  projectsList: Project[];
+  project: Project;
+  getDataComplete = false;
+  constructor() {
+    this.getAllProjects();
+  }
+  getProject(id: number){
+    this.getAllProjects();
+    this.project = this.projectsList.reduce((item) => {
+      if (item.id === id){
+        return item;
+      }
+    });
+  }
+  getAllProjects(){
+    this.projectsList = this.getData();
+  }
+  updateProject(id: number, key: string, value: any){
+    this.getAllProjects();
+    this.projectsList = this.projectsList.map( project =>  project.id === id ? {...project, [key]: value} : project);
+    this.setData(this.projectsList);
+  }
+  addProject(project: Project){
+    this.getAllProjects();
+    this.projectsList.push(project)
+    this.setData(this.projectsList);
+  }
+  deleteProject(id: number){
+    this.getAllProjects();
+    this.projectsList = this.projectsList.filter((item) => item.id !== id);
+    this.setData(this.projectsList);
+  }
+
+  // Common functions for setting and getting data
+  setData(projects: Project[] ){
+    localStorage.setItem('projects', JSON.stringify(projects));
+  }
+  getData(){
+    const projects = localStorage.getItem('projects');
+    if ( projects !== null || projects !== undefined ){
+      return JSON.parse(projects);
+    }
+    return projects;
+  }
+}
