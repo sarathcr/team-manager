f69aa77b948c775efa96ea300cccfa59dd8a6c12
justinthomas0789 <justinthomas0789@gmail.com>
2020-05-06 23:58:48 +0530

    Added a missing semicolon.

--- src/app/modules/creator-home/add-project/add-project.component.ts
+++ src/app/modules/creator-home/add-project/add-project.component.ts
@@ -29,7 +29,7 @@ export class AddProjectComponent implements OnInit {
         'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       ],
       date: new Date()
-    }
+    };
     this.localStorage.addProject(newProject);
   }
 
