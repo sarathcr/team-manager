dba1dfb4c899e1cfc2c1a92e96f99f00d400e231
Abijith John <ajohn@opentrends.net>
2020-05-14 14:10:48 +0530

    Project title integration

--- src/app/modules/project-editor/components/project-input-title/project-input-title.component.ts
+++ src/app/modules/project-editor/components/project-input-title/project-input-title.component.ts
@@ -10,7 +10,6 @@ export class ProjectInputTitleComponent implements OnInit {
   @ViewChild('titleInput') inputElement: ElementRef;
   projectTitle: string;
   showInputfield = false;
-  projectDetails: Project;
   projectId: number;
 
   constructor(private localStorage: LocalStorageService) { }
@@ -19,14 +18,21 @@ export class ProjectInputTitleComponent implements OnInit {
     this.getProjectDetails();
   }
 
+  ngOnDestroy(): void{
+    if (!this.projectTitle) {
+      const noTitle ="Experiencia sin título"  // Default value for no title project
+      this.handleSubmit(this.projectId, 'title', noTitle)
+   }
+  }
+
   // Function to get the project details from service.
   getProjectDetails(): void {
-    this.projectDetails = this.localStorage.project;
-    this.projectId = this.projectDetails.id;
-    if (this.projectDetails.title === '' || this.projectDetails.title === null) {
+    const projectDetails: Project = this.localStorage.project;
+    this.projectId = projectDetails.id;
+    if (!projectDetails.title) {
       this.showInputfield = true;
     } else {
-      this.projectTitle = this.projectDetails.title;
+      this.projectTitle = projectDetails.title;
     }
   }
 
@@ -36,17 +42,20 @@ export class ProjectInputTitleComponent implements OnInit {
     setTimeout(() => this.inputElement.nativeElement.focus(), 0);
   }
 
-  // Function to compare the title value and to save the value.
+  // Function to compare the title value and on blur of input field.
   handleBlur(value: any): void {
     if (this.projectTitle !== value) {
       this.projectTitle = value;
-      this.localStorage.updateProject(this.projectId, 'title', this.projectTitle)
-      if (value !== '' && value !== null) {
+      this.handleSubmit(this.projectId, 'title', this.projectTitle)
+      if (value) {
         this.showInputfield = false;
       }
-    } else {
+    } else if (value) {
       this.showInputfield = false;
     }
   }
-
+// Function saves the title to the backend
+handleSubmit(id: number,key: string,value: any): void{
+  this.localStorage.updateProject(id,key,value)
+}
 }
