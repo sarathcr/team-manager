ffd969c96fce31413fd5fe12946e4ac88ebe4399
Abijith John <ajohn@opentrends.net>
2020-05-19 11:47:39 +0530

    Added localization keys to json file

--- src/app/modules/project-editor/pages/project-editor/project-editor.component.ts
+++ src/app/modules/project-editor/pages/project-editor/project-editor.component.ts
@@ -8,20 +8,6 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class ProjectEditorComponent implements OnInit {
   status='';
-  // title='crea paso apaso';
-  // view='Ver ficha estructura';
-  // items=[
-  //   {id: 1, name:'Punto de partida'},
-  //   {id: 2, name:'Temática'},
-  //   {id: 3, name:'Objetivos competenciales'},
-  //   {id: 4, name:'Contenidos'},
-  //   {id: 5, name:'Evaluación'},
-  //   {id: 6, name:'Título creativo'},
-  //   {id: 7, name:'Preguntas guía'},
-  //   {id: 8, name:'Producto final'},
-  //   {id: 9, name:'Sinopsis'},
-  //   {id: 10, name:'Interacción con alumnos'}
-  // ];
   items: object[];
   constructor(private translate :TranslateService) { }
 
@@ -41,14 +27,14 @@ export class ProjectEditorComponent implements OnInit {
        this.items=[
             {id: 1, name: translations['STEPS_MENU.project_structure_stepsmenu_startingpoint']},
             {id: 2, name: translations['STEPS_MENU.project_structure_stepsmenu_topic']},
-            {id: 3, name:'Objetivos competenciales'},
-            {id: 4, name:'Contenidos'},
-            {id: 5, name:'Evaluación'},
+            {id: 3, name:'Objetivos competenciales'}, // add localization
+            {id: 4, name:'Contenidos'}, // add localization
+            {id: 5, name:'Evaluación'}, // add localization
             {id: 6, name: translations['STEPS_MENU.project_structure_stepsmenu_creativetitle']},
             {id: 7, name: translations['STEPS_MENU.project_stepsmenu_drivingquestion']},
             {id: 8, name: translations['STEPS_MENU.project_structure_stepsmenu_finalproduct']},
             {id: 9, name: translations['STEPS_MENU.project_structure_stepsmenu_sinopsis']},
-            {id: 10, name:'Interacción con alumnos'}
+            {id: 10, name:'Interacción con alumnos'} // add localization
           ];
       }
    );
