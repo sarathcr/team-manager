46df271c32951fa4cf6a4a8e67a4fb8f07743f28
Ratheesh PR <rpadinjareambattu@opentrends.net>
2020-05-19 12:25:30 +0530

    refactor for app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
-import { AppState } from './state';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -17,29 +13,9 @@ export class AppComponent {
   isLoggedIn$: Observable<boolean>;
   isLoggedOut$: Observable<boolean>;
 
-  constructor(
-    private router: Router,
-    private store: Store<AppState>, private http: HttpClient) {
-  }
+  constructor() {  }
 
   ngOnInit() {
-    this.router.events.subscribe(event => {
-      switch (true) {
-        case event instanceof NavigationStart: {
-          this.loading = true;
-          break;
-        }
-
-        case event instanceof NavigationEnd:
-        case event instanceof NavigationCancel:
-        case event instanceof NavigationError: {
-          this.loading = false;
-          break;
-        }
-        default: {
-          break;
-        }
-      }
-    });
   }
+  
 }
