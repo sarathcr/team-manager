eb14bb9d30cfbdced4e3b082d3a9cc90454e93a4
scheerakkadan <scheerakkadan@opentrends.net>
2020-05-21 12:58:29 +0530

    code review changes

--- src/app/modules/project-editor/components/project-title/project-title.component.ts
+++ src/app/modules/project-editor/components/project-title/project-title.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ElementRef, ViewChild, Input, Output, EventEmitter } from '@angular/core';
-import { TitleData } from '../../constants/title-data.model';
+import { ProjectEntityService } from '../../services/project-entity.service';
+import { Project } from 'src/app/shared/constants/project.model';
+import { NewProjectResService } from '../../services/new-project-res.service';
+import { initialProject } from 'src/app/shared/constants/project.data';
 
 @Component({
   selector: 'app-project-title',
@@ -9,40 +12,61 @@ import { TitleData } from '../../constants/title-data.model';
 export class ProjectTitleComponent implements OnInit {
 
   @ViewChild('titleInput') inputElement: ElementRef;
-  @Input() projectData: TitleData;
+  @Input() project: any;
   @Input() maxLength: number;
   @Input() placeholder: any;
-  @Output() titleBlur = new EventEmitter()
-  tempTitle: string;
+  @Output() blur = new EventEmitter()
+  projectTitle: string;
   showInputfield = true;
+  projectId: number;
 
-  constructor() { }
+  constructor(
+    private projectsService: ProjectEntityService,
+    private newProjectRes: NewProjectResService) { }
 
   ngOnInit(): void {
-    if (this.projectData?.title) {
-      this.tempTitle = this.projectData?.title
+    if (this.project?.title) {
       this.showInputfield = false;
     }
   }
 
   // Function to show or hide the input text field.
   toggleInputfield(): void {
-    // this.showInputfield = true;
+    this.showInputfield = true;
     setTimeout(() => this.inputElement.nativeElement.focus(), 0);
   }
 
   // Function to handle blur event of input field.
   handleBlur(event: Event): void {
-    const inputValue = (<HTMLInputElement>event.target).value;
-    this.tempTitle = inputValue;
-    if (inputValue) {
+    const title = (<HTMLInputElement>event.target).value;
+    if (title) {
       this.showInputfield = false;
     } else {
       this.showInputfield = true;
     }
-    if(inputValue || this.projectData?.id){
-      this.titleBlur.emit(event);
+    this.blur.emit(event)
+    // create mode
+    if (!this.project) {
+      if (title == "") return // if not have the value
+      const newProject: Project = {
+        ...initialProject,
+        title
+      }
+      this.projectsService.add(newProject)
+        .subscribe(
+          newProject => {
+            console.log('New Course', newProject);
+            this.newProjectRes.sendResponse(newProject)
+          }
+        );
+    } else {
+      // update mode
+      if (title == this.project.title) return // check if value is same
+      const updateProject = {
+        id: this.project.id,
+        title
+      }
+      this.projectsService.update(updateProject);
     }
   }
-  
 }
