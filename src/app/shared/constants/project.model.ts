9ba2c4e1366f68158e1151831e0349b19f0f3f00
Abijith John <ajohn@opentrends.net>
2020-05-22 10:38:41 +0530

    fixes in project sorting and styles of dropdown,project-editor

--- src/app/shared/constants/project.model.ts
+++ src/app/shared/constants/project.model.ts
@@ -17,10 +17,10 @@ export class Project {
 export function compareProjects(p1: Project, p2: Project) {
   const compare = p1.id - p2.id;
   if (compare > 0) {
-    return 1;
+    return -1;
   }
   else if (compare < 0) {
-    return -1;
+    return 1;
   }
   else return 0;
 }
