b1315f7c80bffb9ceae773cf966b5b5dd47a2c7f
Ratheesh PR <rpadinjareambattu@opentrends.net>
2020-05-29 21:06:02 +0530

    updated Step optional field

--- src/app/modules/project-editor/constants/step.model.ts
+++ src/app/modules/project-editor/constants/step.model.ts
@@ -1,7 +1,7 @@
 export class Step {
     readonly id?: StepId;
-    readonly stepid?: statusId;
-    status?: Status;
+    readonly stepid: statusId;
+    status: Status;
     name?: string;
 }
 
@@ -9,8 +9,3 @@ export type statusId = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9| 10
 export type Status = 'INPROCESS' | 'DONE' | 'PENDING' 
 export type StepId = 'stepOne' | 'stepTwo' | 'stepThree' | 'stepFour' | 'stepFive' | 'stepSix' | 'stepSeven' | 'stepEight' | 'stepNine' | 'stepTen' | 'stepEleven'
 
-// export interface StepMenu {
-//     id: number;
-//     name: string;
-//     status: Status
-// }
