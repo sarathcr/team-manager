fcaad2a0cc1f814947452c739e9205435143e2f5
Abijith John <ajohn@opentrends.net>
2020-06-01 14:11:12 +0530

    Project sorting using created date

--- src/app/shared/constants/project.model.ts
+++ src/app/shared/constants/project.model.ts
@@ -18,15 +18,19 @@ export class Project {
   guideQuestions?: Array<object>;
   grades?: Array<Grade>;
   subjects?: Array<Subject>;
+  createdAt?: string;
+  updatedAt?: string;
 }
 
 export function compareProjects(p1: Project, p2: Project) {
-  const compare = p1.id - p2.id;
-  if (compare > 0) {
-    return -1;
+  const firstTimestamp = new Date(p1?.createdAt).getTime()
+  const secondTimestamp = new Date(p2?.createdAt).getTime()
+    const compare = firstTimestamp - secondTimestamp;
+    if (compare > 0) {
+      return -1;
+    }
+    else if (compare < 0) {
+      return 1;
+    }
+    else return 0;
   }
-  else if (compare < 0) {
-    return 1;
-  }
-  else return 0;
-}
