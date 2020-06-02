4da8a6adbef9ce1172daca1331e8902f90c95304
Abijith John <ajohn@opentrends.net>
2020-06-02 16:02:01 +0530

    Added localization in steps one and two

--- src/app/modules/project-editor/components/textarea/textarea.component.ts
+++ src/app/modules/project-editor/components/textarea/textarea.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 export class TextareaComponent implements OnInit {
 
   @Input() value: string
+  @Input() placeholder: string
   @Output() onChange = new EventEmitter()
   valueModel: string
   constructor() { }
