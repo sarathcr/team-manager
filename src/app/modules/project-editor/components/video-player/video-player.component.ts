037eeecd42de17f505d038d056a82fadc11b2755
justinthomas0789 <justinthomas0789@gmail.com>
2020-06-09 18:58:01 +0530

    Added video component

--- src/app/modules/project-editor/components/video-player/video-player.component.ts
+++ src/app/modules/project-editor/components/video-player/video-player.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, OnDestroy, ElementRef, ViewChild, Input } from '@angular/core';
 import videojs from 'video.js';
 @Component({
   selector: 'app-video-player',
   templateUrl: './video-player.component.html',
-  styleUrls: ['./video-player.component.scss']
+  styleUrls: ['./video-player.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class VideoPlayerComponent implements OnInit {
-
-  constructor() { }
+export class VideoPlayerComponent implements OnInit, OnDestroy {
+  player: videojs.Player;
+  @ViewChild('target', {static: true}) target: ElementRef;
+  // see options: https://github.com/videojs/video.js/blob/mastertutorial-options.html
+  @Input() options: {
+      fluid: boolean,
+      aspectRatio: string,
+      autoplay: boolean,
+      sources: {
+          src: string,
+          type: string,
+      }[],
+  };
+  constructor( private elementRef: ElementRef,) { }
 
   ngOnInit(): void {
+    this.player = videojs(this.target.nativeElement, this.options, function onPlayerReady() {
+      console.log('onPlayerReady', this);
+    });
   }
-
+  ngOnDestroy() {
+    // destroy player
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
 }
