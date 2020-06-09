b9abe58715831b9a6bce30c321e95caab50667be
sarath C R <scheerakkadan@opentrends.net>
2020-06-09 19:17:32 +0530

    Edge character limit fix

--- src/app/modules/project-editor/components/textarea/textarea.component.ts
+++ src/app/modules/project-editor/components/textarea/textarea.component.ts
@@ -5,14 +5,18 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
   styleUrls: ['./textarea.component.scss']
 })
 export class TextareaComponent implements OnInit {
+
   @Input() value: string
   @Input() placeholder: string
   @Input() maxlength: number
   @Output() onChange = new EventEmitter()
   focus = false;
   isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+
   constructor() { }
+
   ngOnInit(): void { }
+
   // Function to get and emit value on textarea
   onValueChange(value: string) {
     if (this.isFirefox && value.length > this.maxlength) {
@@ -21,14 +25,27 @@ export class TextareaComponent implements OnInit {
     this.value = value;
     this.onChange.emit(value.trim());
   }
+
   setFocus() {
     this.focus = true;
   }
+
   onBlur() {
     this.focus = false;
     if (!this.value.trim()) {
       this.value = '';
       this.onChange.emit('');
+    }
+  }
+
+  onKeyDown(event): any {
+    if (!!this.maxlength) {
+      const text = event.target.value;
+      if (text.length > this.maxlength) {
+        // truncate excess text (in the case of a paste)
+        this.value = (text.substring(0, this.maxlength));
+        event.preventDefault();
+      }
     }
   }
 }
