c4738e969c9f3ab4b87d0fd84001817e469761bf
Ratheesh PR <rpadinjareambattu@opentrends.net>
2020-06-24 11:57:16 +0530

    test - 2

---
+++ commit-msg.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+PROJECTS_KEYS=(
+  TA
+)
+
+get_projects_keys () {
+  IFS=$'|'; echo "${PROJECTS_KEYS[*]}"
+}
+
+check_commit_msg () {
+  COMMIT_REGEX="^(($(get_projects_keys))-[0-9]*|NOTASK|Merge branch) [^-]*$"
+  COMMIT_MSG_FILE=$1
+
+  git rev-parse -q --verify MERGE_HEAD && exit 0
+  if ! (grep -iqE "$COMMIT_REGEX" "$COMMIT_MSG_FILE"); then
+    echo "Aborting commit. Your commit message should start with '${PROJECTS_KEY_LIST_REGEX}-' plus the number of your feature or NOTASK or Merge branch" >&2
+    exit 1
+  fi
+}
+
+check_commit_msg "$1"
