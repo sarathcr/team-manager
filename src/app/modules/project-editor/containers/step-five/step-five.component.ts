2c46bad9d822575f17e7f5e48f5d4145fdacf935
bsasieta <bsasieta@opentrends.net>
2020-07-06 14:10:44 +0200

    empezando step 5

--- src/app/modules/project-editor/containers/step-five/step-five.component.ts
+++ src/app/modules/project-editor/containers/step-five/step-five.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core'
+import { Observable } from 'rxjs'
 import { EditorService } from '../../services/editor/editor.service'
-
+import { Step } from '../../constants/step.model'
+import { Project } from '../../constants/project.model'
+import { SubSink } from '../../../../shared/utility/subsink.utility'; import { FieldConfig } from 'src/app/shared/constants/field.model'
 @Component({
   selector: 'app-step-five',
   templateUrl: './step-five.component.html',
   styleUrls: ['./step-five.component.scss']
 })
 export class StepFiveComponent implements OnInit {
+  project$: Observable<Project>
+  step: Step
+  step$: Observable<Step>
+  subscriptions = new SubSink()
+  buttonConfig: FieldConfig
+  project: Project
 
-  constructor(private editor: EditorService) { }
-
-  ngOnInit(): void {
+  constructor(private editor: EditorService) { } ngOnInit(): void {
     this.stepInIt()
   }
 
   stepInIt(): void {
-    this.editor.getDataByStep(5)
+    this.project$ = this.editor.getDataByStep(5)
+    if (this.project$) {
+      this.subscriptions.sink = this.project$.subscribe(data => {
+        if (data) {
+          console.log('competencyObjectives: ', data.competencyObjectives)
+          this.project = data
+        }
+      })
+    }
   }
-
 }
